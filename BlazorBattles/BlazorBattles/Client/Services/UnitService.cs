d24ef3964a6d0e570c59f76f9189326c2ac790fc
Soultan Hatsijev <soultan_98@hotmail.com>
2021-05-12 01:45:33 +0200

    17. Build units with the unit Service

---
+++ BlazorBattles/BlazorBattles/Client/Services/UnitService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorBattles.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public class UnitService : IUnitService
+    {
+        public IList<Unit> Units { get; } = new List<Unit>
+        {
+            new Unit { Id = 1, Title = "Knight", Attack = 10, DefenseValue = 10, BananaCost = 100},
+            new Unit { Id = 2, Title = "Archer", Attack = 15, DefenseValue = 5, BananaCost = 150},
+            new Unit { Id = 3, Title = "Mage", Attack = 20, DefenseValue = 1, BananaCost = 200}
+        };
+
+        public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
+
+        public void AddUnit(int unitId)
+        {
+            Unit unit = Units.First(u => u.Id == unitId);
+            MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
+        }
+    }
+}
