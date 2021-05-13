bfd7baf04a38bcf838f584d12a580ec0bb052bac
Soultan Hatsijev <soultan_98@hotmail.com>
2021-05-13 09:45:27 +0200

    29. The AuthenticationStateProvider

--- BlazorBattles/BlazorBattles/Client/Program.cs
+++ BlazorBattles/BlazorBattles/Client/Program.cs
@@ -1,5 +1,6 @@
 using BlazorBattles.Client.Services;
 using Blazored.Toast;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +26,8 @@ namespace BlazorBattles.Client
 
             builder.Services.AddScoped<IBananaService, BananaService>();
             builder.Services.AddScoped<IUnitService, UnitService>();
+
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
             await builder.Build().RunAsync();
         }
