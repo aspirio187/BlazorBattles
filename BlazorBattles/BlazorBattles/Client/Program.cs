89fdc4cf2c2f563e1e481d5521096b091e1f6397
Soultan Hatsijev <soultan_98@hotmail.com>
2021-05-13 06:23:43 +0200

    21. ToasterMessages with Blazored.Toast

--- BlazorBattles/BlazorBattles/Client/Program.cs
+++ BlazorBattles/BlazorBattles/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorBattles.Client.Services;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +18,8 @@ namespace BlazorBattles.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddBlazoredToast();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
